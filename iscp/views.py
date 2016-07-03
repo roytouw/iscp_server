306af7313c3bfdcb677bc7c17cb84a619d39d014
Roy Touw <roytouw@hotmail.com>
2016-07-03 17:44:19 +0200

    rest call working

--- iscp/views.py
+++ iscp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class TestView(APIView):
+    def get(self, request, *args ,**kw):
+        result = 'hello'
+        response = Response(result, status=status.HTTP_200_OK)
+        return response
