403ad73795ce4f983363e71ce9963ccb08559109
Roy Touw <roytouw@hotmail.com>
2016-07-06 14:13:16 +0200

    tweet analyser working with given search term

--- iscp/views.py
+++ iscp/views.py
@@ -9,7 +9,7 @@ tweetController = TweetController.TweetController()
 
 class StartView(APIView):
     def get(self, request, *args ,**kw):
-        tweetController.search_tweets('islamic state')
+        tweetController.search_tweets(self.kwargs['term'])
         result = ['True']
         response = Response(result, status=status.HTTP_200_OK)
         return response
--- server/urls.py
+++ server/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import patterns, url
 from iscp import views
 
 urlpatterns = patterns('',
-                       url(r'start', views.StartView.as_view(), name='start'),
+                       url(r'start/(?P<term>.+)/$', views.StartView.as_view(), name='start'),
                        url(r'fetch', views.FetchView.as_view(), name='fetch')
                        )
